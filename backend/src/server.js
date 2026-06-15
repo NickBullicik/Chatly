0fecac196dbbddd5883938baf1d674bb62ac2fb8
Nicolás Mazzilli <nicolasmazzilli2004@gmail.com>
2026-06-15 22:16:51 +0200

    vercel config

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,4 +24,8 @@ if(process.env.NODE_ENV === "production") {
     })
 }
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+if (process.env.NODE_ENV !== "production" || process.env.VERCEL !== "1") {
+    app.listen(PORT, () => console.log("Server running on port: " + PORT));
+}
+
+export default app;
