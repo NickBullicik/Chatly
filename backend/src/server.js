a55a52b853fb3e2a1d01828796addc470e3c816c
Nicolás Mazzilli <nicolasmazzilli2004@gmail.com>
2026-06-16 19:44:03 +0200

    fix backend vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
@@ -10,18 +11,23 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// listo para deploy
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
+    app.get("*", (req, res) => {
+        if (req.path.startsWith("/api")) {
+            return res.status(404).json({ error: "Ruta no encontrada" });
+        }
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
 if (process.env.NODE_ENV !== "production" || process.env.VERCEL !== "1") {
